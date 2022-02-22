22e6fe4c6b5df16679251c1019fac7b48794d790
Mischel <kandiahmischel@gmail.com>
2022-02-23 00:09:37 +1030

    Added JSDOM for performance calculation and tidied up

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// Needs for performance tools
+const { JSDOM } = require("jsdom")
+const { window } = new JSDOM()
+
 // Simple fibonacci calculator
-const fibSlow = n => {
+const fibSimple = n => {
   if (n <= 1 ) return n;
 
   let prev = 0;
@@ -13,22 +17,37 @@ const fibSlow = n => {
 };
 
 // recursive Fib(n);
-const fib = n => {
+const fibRecursive = n => {
   if (n <=2) return 1;
-  return fib(n - 1) + fib(n - 2);
+  return fibRecursive(n - 1) + fibRecursive(n - 2);
 };
 
 // recursive Fib(n) but very efficient. Store the previous calculated values in an object.
-const fibFast = (n, mem = {}) => {
+const fibMemory = (n, mem = {}) => {
   if(n in mem) return mem[n];
   if (n <=2) return 1;
-  mem[n] = fibFast(n - 1, mem) + fibFast(n - 2, mem);
+  mem[n] = fibMemory(n - 1, mem) + fibMemory(n - 2, mem);
   return mem[n];
 };
 
+// fib target element
+let n = 100;
 
-let n = 20;
+let start = window.performance.now();
 
-// console.log(fibSlow(n));
-// console.log(fib(n));
-console.log(fibFast(n));
+switch (1) {
+  case 1:
+    console.log(fibSimple(n));
+    break;
+  case 2:
+    console.log(fibRecursive(n));
+    break;
+  case 3:
+    console.log(fibMemory(n));
+    break;
+  default:
+    console.log("Please specify a valid method: simple = 1, recursive = 2, or memo = 3");
+}
+
+let end = window.performance.now();
+console.log(`Time: ${(end - start)} milliseconds`);
